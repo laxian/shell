32013e1afca3a5e1bc84ed4d58e830626f629102
zhouweixian <laxian2010@qq.com>
2018-06-11 23:05:55 +0800

    finish the replace by regex python script

--- dp_replace.py
+++ dp_replace.py
@@ -3,15 +3,8 @@
 import sys
 import re
 
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="346dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
 
-input_file = open(path, 'r+')
-
+__author__ = 'zwx'
 
 def procceed_line(line):
     m = p.search(line)
@@ -25,256 +18,24 @@ def procceed_line(line):
         # print ("not found")
         return line
 
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
 
-input_file.close()
+if __name__ == '__main__':
 
-# procceed_line(test_line)
-#!/usr/bin/python
+    # 从命令行读取被处理文件路径
+    path = sys.argv[1]
+    # test_line = 'android:layout_width="346dp"'
 
-import sys
-import re
+    # 正则，匹配格式： 123.24dp
+    r = '(?P<num>\d+(\.\d+)?)dp'
+    p = re.compile(r)
 
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="519.0dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
+    input_file = open(path, 'r+')
+    lines = input_file.readlines()
+    input_file.truncate()
+    input_file.seek(0)
+    for l in lines:
+        newline = procceed_line(l)
+        print(newline)
+        input_file.write(newline)
 
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="519.0dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="778.5dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="519.0dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="778.5dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="778.5dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
-#!/usr/bin/python
-
-import sys
-import re
-
-path = sys.argv[0]
-# regex = sys.argv[1]
-# replacement = sys.argv[2]
-test_line = 'android:layout_width="1167.75dp"'
-r = '(?P<num>\d+(\.\d+)?)dp'
-p = re.compile(r)
-
-input_file = open(path, 'r+')
-
-
-def procceed_line(line):
-    m = p.search(line)
-    if m is not None:
-        num = m.group('num')
-        # print ('%s -> %.1f' % (num, float(num) * 1.5))
-        newline = line.replace(num, str(float(num) * 1.5))
-        # print(newline)
-        return newline
-    else:
-        # print ("not found")
-        return line
-
-for l in input_file.readlines():
-    newline = procceed_line(l)
-    print(newline)
-    input_file.write(newline)
-
-input_file.close()
-
-# procceed_line(test_line)
+    input_file.close()
