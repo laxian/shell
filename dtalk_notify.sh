4c42dcf94b44d747676e8c5bf7db8f978a0b7c5f
laxian <zhouweixian@etiantian.com>
2019-04-30 18:27:57 +0800

    完善命令行通知脚本

--- dtalk_notify.sh
+++ dtalk_notify.sh
@@ -1,6 +1,6 @@
 
 dingtalk=false
-pc=false
+pc=true
 name=""
 at=""
 while [[ $# > 0 ]]; do
@@ -27,6 +27,7 @@ while [[ $# > 0 ]]; do
         ;;
     *) # unknown flag/switch
         POSITIONAL+=("$1")
+        name=$POSITIONAL
         shift
         ;;
     esac
@@ -35,13 +36,13 @@ done
 set -- "${POSITIONAL[@]}" # restore positional params
 
 function dingtalk_notify() {
-    curl 'https://oapi.dingtalk.com/robot/send?access_token=xxx' \
+    curl 'https://oapi.dingtalk.com/robot/send?access_token=d7fb2719a1655eb9e067dd549a86385cc7f57e14a056fe52187da85c1adf3159' \
         -H 'Content-Type: application/json' \
-        -d "{ \"msgtype\": \"text\", \"text\": { \"content\": \"您的任务已完成: $1\" }, \"at\": { \"atMobiles\": [ \"18310511388\" ], \"isAtAll\": false } }"
+        -d "{ \"msgtype\": \"text\", \"text\": { \"content\": \"您的任务已完成: $*\" }, \"at\": { \"atMobiles\": [ \"18310511388\" ], \"isAtAll\": false } }"
 }
 
 function pc_notify () {
-    osascript -e "display notification \"${strPrompt}\" with title \"$1 完成\""
+    osascript -e "display notification \"${strPrompt}\" with title \"$* 完成\""
 
 }
 
