bef44c89970854bb86ccb79ff810b7a6f46da6b7
weixian.zhou <weixian.zhou@ninebot.com>
2020-04-08 20:00:39 +0800

    save

--- apkviewer/setup.sh
+++ apkviewer/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-d2j_url=https://github.com/pxb1988/dex2jar/files/1867564/dex-tools-2.1-SNAPSHOT.zip
-apktool_url=https://connortumbleson.com/apktool/apktool_2.4.1.jar
+d2j_url=https://jaist.dl.sourceforge.net/project/dex2jar/dex2jar-2.0.zip
+apktool_url=https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar
 jadx_url=https://github.com/skylot/jadx/releases/download/v1.1.0/jadx-1.1.0.zip
 
-curl -s0 $j2j_url
-curl -s0 $apktool_url
-curl -s0 $jadx_url
+curl -O $d2j_url
+curl -O $jadx_url
+curl -O $apktool_url
