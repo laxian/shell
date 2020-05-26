f0e49acc0dd02c56849ce43cac11fc1173e10b80
= <=>
2020-05-26 16:07:54 +0800

    add capitalize; fix some error

--- apr/cube/env/ENV.sh
+++ apr/cube/env/ENV.sh
@@ -13,13 +13,13 @@ CONFIG_FILE='{"NavServicURL":"SSL_PATH","EnableLog":true}'
 HOST_PATTERN="https\?://[a-z-]\+\.lllll\.com"
 CONFIG_PATH=/sdcard/GX/config.json
 
-HOST_ALPHA=http://alpha-cube.lllll.com
+HOST_ALPHA=https://alpha-cube.lllll.com
 SSL_ALPHA=ssl://000.92.80.43:8884
-HOST_DEV=http://dev-cube.lllll.com
+HOST_DEV=https://dev-cube.lllll.com
 SSL_DEV=ssl://000.131.2.136:8884
-HOST_INTERNAL=http://internal-cube.lllll.com
+HOST_INTERNAL=https://internal-cube.lllll.com
 SSL_INTERNAL=ssl://000.131.2.136:8885
-HOST_RELEASE=http://cube.lllll.com
+HOST_RELEASE=https://cube.lllll.com
 SSL_RELEASE=ssl://000.131.12.142:8884
 # alpha debug release
 APK_DIR=alpha
