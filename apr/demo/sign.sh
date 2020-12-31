c4f316a7f662cb996240598fad641ce79a23ea94
= <=>
2020-12-31 23:51:45 +0800

    fix sign anywhere

--- apr/demo/sign.sh
+++ apr/demo/sign.sh
@@ -4,5 +4,7 @@ workdir=$(
     cd $(dirname $0)
     pwd
 )
+echo $workdir
+parent=`dirname $workdir`
 
-java -jar $workdir/../files/signapk.jar $/workdir/../private/rk/platform.x509.pem $workdir/../private/rk/platform.pk8 $1 $2
+java -jar $parent/files/signapk.jar $parent/private/rk/platform.x509.pem $parent/private/rk/platform.pk8 $1 $2
