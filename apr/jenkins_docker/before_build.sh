e2347ea5fb4308229cec14fc81527af7c8dfb758
laxian <laxian2010@qq.com>
2021-11-17 16:18:34 +0800

    fix nav build

--- apr/jenkins_docker/before_build.sh
+++ apr/jenkins_docker/before_build.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash -x
 
 echo "------ before gradle build ------"
-if [ $use_aliyun_maven == 'true' ]; then
+if [ $use_aliyun_maven = true ]; then
     sed -i "/repositories {/a \ \t\tmaven { url 'https://maven.aliyun.com/repository/google' }" build.gradle
     sed -i "/repositories {/a \ \t\tmaven { url 'https://maven.aliyun.com/repository/jcenter' }" build.gradle
 fi
 if [ $JOB_NAME == "Gx_Service" ]; then
     sed -i "s;3.+;5.3.0;g" build.gradle
     echo "/opt/app/android-sdk/ndk/22.0.7026061" >> local.properties
+fi
+if [ $JOB_NAME == "nav_app-debug" ]; then
+    echo "sdk.dir=/opt/app/android-sdk" > local.properties
+    echo "ndk.dir=/opt/app/android-sdk/ndk/16.1.4479499" >> local.properties
+    git submodule update --init --recursive
+    python3 -m pip install -r ./sdk/trident_ftp/requirements.txt
+    ./gradlew sdk:algobase:assemble
 fi
 
 GIT_COUNT=$(git rev-list HEAD --first-parent --count)
