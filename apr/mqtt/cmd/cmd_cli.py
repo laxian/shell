14fcddd9a09d4edce94daa39c02efa370dc9d5eb
laxian <laxian2010@qq.com>
2023-09-04 17:01:36 +0800

    add -v --verbose

--- apr/mqtt/cmd/cmd_cli.py
+++ apr/mqtt/cmd/cmd_cli.py
@@ -65,32 +65,46 @@ parser = argparse.ArgumentParser(description='处理参数')
 # 添加选项参数
 parser.add_argument('-r', '--robot', help='指定一个机器')
 parser.add_argument('-c', '--command', help='指定一个命令')
-parser.add_argument('-t', '--timeout', help='超时时间s')
+parser.add_argument('-t', '--timeout', help='超时时间s', default=10)
+parser.add_argument('-v', '--verbose', help='输出详细日志', nargs='?', const=True, default=False)
+
+
+verbose = False
 
 # 解析命令行参数
 args = parser.parse_args()
 
+def log(msg, *args, **kw):
+    if verbose:
+        print(msg, args, kw)
+
 # 检查并使用选项参数
 if args.robot is not None:
-    print('指定的机器:', args.robot)
+    log('指定的机器:', args.robot)
     robots = args.robot.split(',')
 else:
     print("robot is not specified")
     exit(1)
 
 if args.command is not None:
-    print('指定的命令:', args.command)
+    log('指定的命令:', args.command)
     command = args.command.replace("\r\n", "\n")
 else:
     print("command is not specified")
     exit(1)
 
 if args.timeout is not None:
-    print('超时时间:', args.timeout)
+    log('超时时间:', args.timeout)
     timeout = int(args.timeout)
 else:
-    print("timeout is not specified，default：10s")
+    log("timeout is not specified，default：10s")
     timeout = 10
+
+if args.verbose:
+    log('输出详细日志:', args.verbose)
+    verbose = True
+else:
+    log("verbose is not specified，default：false")
 
 commands = [
     # "cat data/data/com.segway.robotic.app/shared_prefs/sp_preferences.xml|grep preference_key_admin_password",
@@ -103,13 +117,13 @@ default_pos =  "input tap 650 450;input tap 650 450;input tap 650 450;input tap 
 
 # 连接回调
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
+    log(f"Connected with result code {rc}")
     # 订阅主题
     # client.subscribe(sub_topic)
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print(f"---------------- Subscribed {client} {userdata} {mid} {granted_qos} {sub_topic} ----------------")
+    log(f"---------------- Subscribed {client} {userdata} {mid} {granted_qos} {sub_topic} ----------------")
     send_verify()
 
 
@@ -119,8 +133,8 @@ def on_message(client, userdata, msg):
     json_object = json.loads(msg.payload.decode("utf-8"))
     content = json_object["responce"]
     if json_object["commandType"] == "verified":
-        print("---------------- VERIFIED ----------------")
-        print(json_object["responce"])
+        log("---------------- VERIFIED ----------------")
+        log(json_object["responce"])
 
         with open(pri_key_path, "rb") as f:
             private_key = f.read()
@@ -129,42 +143,42 @@ def on_message(client, userdata, msg):
             json_object["responce"],
             private_key,
         )
-        print(aeskey)
+        log(aeskey)
         send_message(client, commands[index])
         index += 1
     elif json_object["commandType"] == "doRSA":
-        print("---------------- doRSA ack ----------------")
+        log("---------------- doRSA ack ----------------")
         content = json_object["responce"]
-        # print(content)
+        # log(content)
 
     elif json_object["commandType"] == "execute":
         if content.startswith("start handle message"):
-            print("---------------- execute ack ----------------")
+            log("---------------- execute ack ----------------")
             # raw = content.split("'")
             # aes = raw[5].strip()
-            # print(f"AES: {aes}")
+            # log(f"AES: {aes}")
             # dec = aes_decrypt_java(aes, aeskey)
-            # print(f"Decrypted: {dec}")
+            # log(f"Decrypted: {dec}")
         else:
-            print("---------------- execute ----------------")
+            log("---------------- execute ----------------")
             print(content)
             if index >= len(commands):
                 clean_and_unsubscribe()
                 index = 0
             else:
                 # time.sleep(1)
-                print(f'index is: {index}')
+                log(f'index is: {index}')
                 send_message(client, commands[index])
                 index += 1
             
 
     else:
-        print(f"Received message: {json_object['commandType']}")
+        log(f"Received message: {json_object['commandType']}")
 
 
 def clean_and_unsubscribe():
     global sub_topic
-    print(f"---------------- UNSUB {sub_topic} ----------------")
+    log(f"---------------- UNSUB {sub_topic} ----------------")
     client.unsubscribe(sub_topic)
     global robot_id, pub_topic
     sub_topic = sub_topic.replace(robot_id, "{robotId}")
@@ -177,10 +191,10 @@ def make_message(cmd):
     inner = CmdMessageInner()
     inner.set_command(cmd)
     inner_str = json.dumps(inner.to_dict())
-    print(inner_str)
+    log(inner_str)
     en_inner_str = aes_encrypt_java(inner_str, aeskey)
-    print(f"Encrypted inner message: {en_inner_str}")
-    print(en_inner_str)
+    log(f"Encrypted inner message: {en_inner_str}")
+    log(en_inner_str)
     message_payload = CmdMessage(command_type="execute", cmd_message_inner=en_inner_str)
     return message_payload
 
@@ -188,22 +202,22 @@ def make_message(cmd):
 def send_message(client, msg):
     message_payload = make_message(msg)
     payload = json.dumps(message_payload.to_dict())
-    print(f"================================ SEND {robot_id} ===================================")
-    print(payload)
-    print("============================== SEND END =================================")
+    log(f"================================ SEND {robot_id} ===================================")
+    log(payload)
+    log("============================== SEND END =================================")
     client.publish(pub_topic, payload=payload, qos=1)
 
 
 def send_verify():
     # 发布消息
     # message_payload = '{"commandId":1,"commandType":"doRSA","mCmdMessageInner":"pwd"}'
-    print(f"send message {pub_topic}")
+    log(f"send message {pub_topic}")
     message_payload = CmdMessage(command_type="doRSA")
     hash = str(uuid.uuid4())
     # hash = '4558ac1466f54c13'
     inner = CmdMessageInner()
     inner.set_un_signed_string(hash)
-    # print(base64.b64encode(rsa_sign_sha1(private_key=prikey, data=hash)).decode("utf-8"))
+    # log(base64.b64encode(rsa_sign_sha1(private_key=prikey, data=hash)).decode("utf-8"))
     inner.set_signed_string(
         base64.b64encode(rsa_sign_sha1(private_key=prikey, data=hash)).decode("utf-8")
     )
@@ -224,8 +238,8 @@ def subscribe_robot(robot_id):
     global sub_topic, pub_topic
     sub_topic = sub_topic.replace("{robotId}", robot_id)
     pub_topic = pub_topic.replace("{robotId}", robot_id)
-    print(f"Subscribing to {sub_topic} {robot_id}")
-    print(f"Publishing to {pub_topic}")
+    log(f"Subscribing to {sub_topic} {robot_id}")
+    log(f"Publishing to {pub_topic}")
     client.subscribe(sub_topic)
 
 
@@ -263,26 +277,25 @@ if __name__ == "__main__":
     # 持续运行，等待消息
     try:
         for robot in robots:
-            print('\n')
-            print('\n')
-            print('---> handle robot: ' + robot)
+            log('\n')
+            log('\n')
+            log('---> handle robot: ' + robot)
             robot_id = robot
             subscribe_robot(robot)
             cnt = 0
             while robot_id is not None:
                 try:
                     time.sleep(1)
-                    print(".")
                     cnt += 1
                     if cnt > timeout:
                         clean_and_unsubscribe()
                         break
+                    log(".")
                 except KeyboardInterrupt:
                     print(f"Interrupted by user, skipping...{robot_id}")
                     clean_and_unsubscribe()
                     break
-        print(f"Finished!")
+        log(f"Finished!")
     except KeyboardInterrupt:
         close_connection()
         print("Disconnected and stopped the loop.")
-        print(aeskey)
